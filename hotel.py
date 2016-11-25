1de11fc1fd5359eee7872a621f82055698c4092c
felix314159 <felix.rhm@gmx.de>
2016-11-25 10:13:56 +0100

    Add files via upload

---
+++ hotel.py
@@ -0,0 +1,45 @@
+from random import randint
+
+def new_day():
+    print("""\n
+    -------------------------------------------
+    -------------------------------------------""")
+    print("""                        Menu
+    -------------------------------------------
+    s --> Skip. Don't do anything today. \n
+    b --> Build a new hotel. \n
+    m --> Move manager to neighboured city. \n
+    h --> Hire a new manager. Takes three days!
+    -------------------------------------------
+                    End of Menu
+    -------------------------------------------
+    -------------------------------------------""")
+
+    selection = input("    Please choose what to do: ")
+
+    if selection == "s":
+        pass
+    elif selection == "b":
+        pass
+    elif selection == "m":
+        pass
+    elif selection == "h":
+        pass
+    else:
+        print("Invalid input! Please try again.")
+
+def set_startvalues(n, m, d, p_i, s_k_l):
+    # irgendwie müssen wir die Funktionsparameter optional machen,
+    # vielleicht so wie hier:
+    # http://stackoverflow.com/questions/14749328/python-how-to-check-whether-optional-function-parameter-is-set
+
+    '''
+    Soll der User Städtenamen eingeben? Wie genau soll der
+    User denn die Straßen festlegen, wir übergeben ja die
+    s_k_l Variable, aber ?
+    '''
+
+    n = str(randint(5, 20))
+    print(n)
+
+set_startvalues(2, 4, 5, -10, 0)
